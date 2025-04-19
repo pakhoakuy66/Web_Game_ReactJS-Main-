08433f9344f6b3eb6c67b07fae3f19f78dbfa24b
pakhoakuy66 <phananhkhoa41004@gmail.com>
2025-04-19 15:26:21 +0700

    rename app

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./07.PhanAnhKhoa";
+import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
